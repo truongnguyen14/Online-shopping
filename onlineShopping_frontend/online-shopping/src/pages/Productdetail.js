e86ef7a5c23d96b66d1d7f51d86ba7cf7d0268b4
truongnguyen14 <80660271+truongnguyen14@users.noreply.github.com>
2022-09-02 14:33:57 +0700

    Add files via upload

---
+++ onlineShopping_frontend/online-shopping/src/pages/Productdetail.js
@@ -0,0 +1,42 @@
+import React, {useContext,useState, useEffect} from 'react'
+import {useParams, Link} from 'react-router-dom'
+import {GlobalState} from '../../GlobalState'
+import './productdetail.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import ProductItem from './util/productitem/ProductItem'
+
+function Productdetail() {
+
+  return (
+    <div>
+        <div class="productinfo container">
+            <div class="product">			
+                        <div class="row">
+                        <div class="col-md-5 box1">
+                            <img src="#" class="poster-image"></img>
+                        </div>
+                        <div class="col align-self-center box2">
+                            <h1 class="productinfo-title">'.$row['name'].'</h1>
+                            <h5>Description: '.$row['moviedescription'].'</h5>
+                            <h5>Genre: '.$row['genre'].'</h5>
+                            <h5>Rated: '.$row['rated'].'</h5>
+                            <h5>Rating: '.$row['rating'].'</h5>
+                            <div class="productinfo button">
+                                <a href="#" class="btn btn-primary">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>			
+            </div>
+	    </div>
+    
+    <div>
+        <h2>Related products</h2>
+        <div className='products'>
+           
+        </div>
+    </div></div>
+   
+  )
+}
+
+export default Productdetail
