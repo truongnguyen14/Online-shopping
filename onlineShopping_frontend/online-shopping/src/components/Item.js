2d0b4db6f6ff334b95d1ba0d2453e4cf78297f9c
truongnguyen14 <80660271+truongnguyen14@users.noreply.github.com>
2022-09-06 03:31:51 +0700

    Update Item.js

--- onlineShopping_frontend/online-shopping/src/components/Item.js
+++ onlineShopping_frontend/online-shopping/src/components/Item.js
@@ -6,14 +6,21 @@ import './item.css'
 export default function Item(props){
     const {product,onAdd} = props 
     return(
-        <div className="item">
-             <img className="item-image" src={product.image} alt="this is a showes of an item" />
-             <div className="item-sale">Sale up to 50%</div>
-             <Link to= {`/product/find/${product.id}`} className="item-description"              >
-                   <div>{product.name}</div>     
-                    </Link>
-             <div className="price red">$ {product.price}</div>
-             <button className="btn btn-primary" onClick={()=>onAdd(product)}>Add to cart</button>
-        </div>
+        <div className="container">
+            <div className="row">
+            <div className="col-md-4">
+                <div className="item">
+                    <img className="item-image" src={product.image} alt="this is a showes of an item" />
+                <div className="card-body">
+                    <div className="item-sale">Sale up to 50%</div>
+                        <Link to= {`/product/find/${product.id}`} className="item-description"              >
+                                <div>{product.name}</div>     
+                        </Link>
+                    <div className="price red">$ {product.price}</div>
+                    <button className="btn btn-primary" onClick={()=>onAdd(product)}>Add to cart</button>
+                </div>
+            </div>
+        </div>        
+    </div> </div>   
     )
 }
