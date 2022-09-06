bbe8c9cc31db8257aecdd1c7e9bc10698b858801
s3812648 <s3812648@rmit.edu.vn>
2022-09-07 02:17:33 +0700

    login logout register authentication

---
+++ onlineShopping_backend/routes/userRouter.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const userCtrl = require('../controllers/userCtrl')
+const auth = require('../middlewares/auth')
+
+router.post('/register', userCtrl.register)
+
+router.post('/login', userCtrl.login)
+
+router.get('/refresh_token', userCtrl.refreshToken)
+
+router.get('/logout', userCtrl.logout)
+
+router.get('/infor', auth, userCtrl.getUser)
+module.exports = router
